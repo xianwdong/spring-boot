dd6cbbbb6c7df0fc1067c803f4acd16448d8e629
xianwdong <xianwdong@163.com>
2020-09-11 15:45:26 +0800

    增加profile测试

--- spring-boot-project/spring-boot-learn/src/main/java/com/test/controller/IndexController.java
+++ spring-boot-project/spring-boot-learn/src/main/java/com/test/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+	@Autowired
+	private Environment environment;
+
 	@GetMapping("/index")
 	public String index() {
+		/**
+		 * 测试加的参数
+		 * vm参数 -Dspring.profiles.active=dev,test -Dhello3=world3
+		 * program arguments
+		 * --hello2=world2
+		 * --spring.application.json={\"foo\":\"bar\"}
+		 * --spring.proflies.active=test
+		 * */
+		System.out.println(environment.getProperty("hello"));
+		System.out.println(environment.getProperty("hello3"));
+		System.out.println(environment.getProperty("profile"));
+		System.out.println(environment.getProperty("profile2"));
 		return "hello world";
 	}
 
